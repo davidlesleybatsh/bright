a90c56aee125629005d30a7d73fb43fe9fdb0020
davidlesleybatsh <davidbaley14@gmail.com>
2023-12-02 00:15:33 +0100

    poc to rob,change2Python

--- index.js
+++ index.js
@@ -3,34 +3,41 @@ import * as cheerio from 'cheerio';
 import express, { response } from "express";
 import getenv from "getenv";
 
-// const env = getenv();
+// const env = getenv(); fix it, Felix Junior, Ralphs Wrecking
 const PORT = 3000;
-let search = "";
-
 const app = express();
 
-const url = `https://www.kleinanzeigen.de/s-umzug-berlin/k0`;
-// const url = `https://www.theguardian.com/uk`;
+try {
+    const axioResponse = await axios.request({
+        method: "GET",
+        url: "https://www.kleinanzeigen.de",
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    })
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const articles = [];
 
-axios(url)
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const articles = [];
+            console.log("Show me some Html", html)
 
-        console.log("Show me some Html", html)
+            // just Any Class. POC - Ebay does pre/re/MagicAgain/ChangeDOM
+            $('.position-relative', html).each(function () {
+                const title = $(this).text();
+                const link = $(this).find('h3').attr('href');
 
-        $('.SOME__CLASS', html).each(function() {
-            const title = $(this).text();
-            const link = $(this).find('a').attr('href');
-
-            articles.push({
-                title,
-                link
+                console.log("Articles", html);
+                articles.push({
+                    title,
+                    link
+                })
             })
-        })
-        console.log("Articles", articles);
-    }).catch(err => console.log(err));
+            console.log("Articles", articles);
+        }).catch(err => console.log(err));
 
+} catch {
+    err => console.log(err);
+}
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
